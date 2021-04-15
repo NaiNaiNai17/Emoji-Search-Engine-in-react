b9ce4786fbada922ea06372b0ffec0b2b8631796
NaiNaiNai17 <fowler.naimah@gmail.com>
2021-04-15 11:02:33 +0200

    getting json data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-
 import React from 'react';
+import Header from './components/header';
+import emojilist from './json/emoji.json';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = { searchValue: '' };
+
+  grabValueFromInput = (e) => {
+    console.log('user is typing');
+
+    //store input data into state
+    this.setState({
+      searchValue: e.target.value,
+    });
+  };
+
+  render() {
+    console.log('render in app', this.state.searchValue);
+    return (
+      <div
+        className='App'
+        style={{
+          border: '.5px solid #3772FF',
+          backgroundColor: '#70E4EF',
+          height: '200px',
+          padding: '20px',
+          color: '#EF709D',
+          textShadow: '.5px .7px  .3px #F038FF',
+        }}
+      >
+        <Header />
+
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <input
+            type='text'
+            style={{
+              fontSize: '20px',
+              width: '400px',
+              padding: '5px',
+              borderStyle: 'none',
+              borderRadius: '5px',
+              boxShadow: '.5px .5px #F038FF',
+            }}
+            onChange={this.grabValueFromInput}
+          />
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default App;

97b4046c0329d39d6fd7cf6e8a80e36b3ac659e3
NaiNaiNai17 <fowler.naimah@gmail.com>
2021-04-15 12:03:29 +0200

    title changed

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import GitHubButton from 'react-github-btn';
 export default function Header() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Search Emoji &#127752;</h1>
+      <h1>Emoji Finder &#127752;</h1>
       <p>Emoji search engine with ReactJS</p>
       <p>
         <GitHubButton

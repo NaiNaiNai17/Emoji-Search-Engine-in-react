99461c7b0439403af43e158709688656491e71b0
NaiNaiNai17 <fowler.naimah@gmail.com>
2021-04-15 12:14:53 +0200

    added new font and color

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ import GitHubButton from 'react-github-btn';
 export default function Header() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Emoji Finder &#127752;</h1>
-      <p>Emoji search engine with ReactJS</p>
+      <h1 style={{ fontSize: '3rem' }}>Emoji Finder &#127752;</h1>
+      <p style={{ fontSize: '1.3rem' }}>Emoji search engine with ReactJS</p>
       <p>
         <GitHubButton
           href='https://github.com/NaiNaiNai17'
